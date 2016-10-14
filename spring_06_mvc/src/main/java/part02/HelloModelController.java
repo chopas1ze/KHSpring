0fa301554ac3df7643f36735301fcf445a71784f
chopas1ze <chopas1ze@gmail.com>
2016-10-14 15:08:29 +0900

    10/14 commit

---
+++ spring_06_mvc/src/main/java/part02/HelloModelController.java
@@ -0,0 +1,20 @@
+package part02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// http://localhost:8090/springmvc/view/part02/HelloModel.htm
+@Controller
+public class HelloModelController {
+//이방법은 사용하진 않으니 잊길바람
+	@RequestMapping("/view/part02/HelloModel.htm")
+	public Model search(){
+		Model model = new ExtendedModelMap();
+		model.addAttribute("id","korea");
+		return model;
+	}
+	
+	
+}//end class
